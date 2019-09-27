1b21dfd59a32743218f477c58a4d6e50e14b5513
fakorede <abiolafakorede@gmail.com>
2019-09-27 02:41:21 +0100

    created all endpoints

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ const app = express()
 
 app.use(express.json())
 
+const genres = [
+
+]
+
+// endpoints
+app.get('/genres', (req, res) => {
+
+})
+
+app.post('/genres', (req, res) => {
+
+})
+
+app.put('/genres/:id', (req, res) => {
+
+})
+
+app.delete('/genres/:id', (req, res) => {
+
+})
+
 const port = process.env.PORT || 5001
 
 app.listen(port, () => {
