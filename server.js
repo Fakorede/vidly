c8dfb2f5b8dc6cd80cd7f635b5626f3ebb7d1c80
fakorede <abiolafakorede@gmail.com>
2019-09-30 00:32:09 +0100

    implemented endpoints for the rentals

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to mongoDB...'))
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
+app.use('/api/rentals', rentals)
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Vidly Api!")
