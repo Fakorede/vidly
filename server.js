7ad4b74951b54e0b425fe3464c450f522f66bbbb
fakorede <abiolafakorede@gmail.com>
2019-10-02 13:53:57 +0100

    implemented express error middleware and express-async errors

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const config = require('config')
 const mongoose = require('mongoose')
 const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 const express = require('express')
+require('express-async-errors')
 
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
+const error = require('./middlewares/error')
+
+if (!config.get('jwtPrivateKey')) {
+    console.log('FATAL ERROR: jwtPrivateKey is not defined!')
+    process.exit(1)
+}
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to mongoDB...'))
@@ -21,6 +30,8 @@ app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
+app.use(error)
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Vidly Api!")
