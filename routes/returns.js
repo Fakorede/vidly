1520dea0ab2adb182ce3782280004f50e91bd023
fakorede <abiolafakorede@gmail.com>
2019-10-09 03:22:56 +0100

    testing the returns

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,34 @@
+const moment = require('moment')
 const express = require('express')
 const router = express.Router()
 
+const auth = require('../middlewares/auth')
 const { Rental, validate } = require('../models/Rental')
+const { Movie, validate } = require('../models/Movie')
 
-router.post('/', async (req, res) => {
-    res.status(401).send('unauthorized!')
+router.post('/', auth, async (req, res) => {
+    if (!req.body.customerId) return res.status(400).send('customerId not provided!')
+    if (!req.body.movieId) return res.status(400).send('movieId not provided!')
 
-    const rental = new Rental({
-        
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
     })
+
+    if (!rental) return res.status(404).send('Rental not found!')
+
+    if (rental.dateReturned) return res.status(400).send('Return already processed!')
+
+    rental.dateReturned = new Date()
+    const rentalDays = moment().diff(rental.dateOut, 'days')
+    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate
+    await rental.save()
+
+    await Movie.update({ _id: rental.movie._id }, {
+        $inc: { numberInStock: 1 }
+    })
+
+    return res.status(200).send(rental)
 })
 
 module.exports = router
